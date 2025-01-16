072889b1f1b0de8bcbf8c9939fcfced09ffacae7
HumbleRobo <lucky038038933@gmail.com>
2025-01-16 16:55:08 +0800

    Updates

---
+++ precache-manifest.3199555de47e0a5bded2375dabcc6e6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862acbabd5a9167278d08c256fe2cc70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f61bfe0da55fb384f62",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "104425faa6b39e7e3561",
+    "url": "/static/js/2.6c04e403.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/static/js/2.6c04e403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2474da85dfd58ca7358a",
+    "url": "/static/js/3.911f07e6.chunk.js"
+  },
+  {
+    "revision": "8f61bfe0da55fb384f62",
+    "url": "/static/js/main.0e86c9f1.chunk.js"
+  },
+  {
+    "revision": "a9b6a47d6d79fefa8182",
+    "url": "/static/js/runtime-main.d83286ad.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
